461157ee7ab6486d24adbe6227729f3a56e9ffe2
nikhilpatil29 <patilnikhil274@gmail.com>
2018-10-15 12:43:40 +0530

    recent commit

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -4,4 +4,4 @@ cd /home/ec2-user/myApplication/project-venv/
 source /home/ec2-user/myApplication/project-venv/bin/activate
 #echo yes | DJANGO_SETTINGS_MODULE=project.settings.staging SECRET_KEY=your-secret-here /home/ec2-user/www/project/manage.py collectstatic
 #DJANGO_SETTINGS_MODULE=project.settings.staging SECRET_KEY=your-secret-here  -c /home/ec2-user/www/project/supervisor/default.conf
-python3 /home/ec2-user/myApplication/project-venv/simpleDjangoApp/manage.py runserver 0.0.0.0:8080
+nohup python3 /home/ec2-user/myApplication/project-venv/simpleDjangoApp/manage.py runserver </dev/null &>/dev/null &
