2e1f9bc835562d470178dbe9d1af2bcf1492115e
nikhilpatil29 <patilnikhil274@gmail.com>
2018-10-15 11:48:39 +0530

    15/10 newly updated

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/myApplication/project-venv/simpleDjangoApp
+cd /home/ec2-user/myApplication/project-venv/
 source /home/ec2-user/myApplication/project-venv/bin/activate
+git clone https://github.com/nikhilpatil29/simpleDjangoApp.git
 python3 /home/ec2-user/myApplication/project-venv/simpleDjangoApp/simpleDjangoApp/manage.py makemigrations
 python3 /home/ec2-user/myApplication/project-venv/simpleDjangoApp/simpleDjangoApp/manage.py migrate auth
 python3 /home/ec2-user/myApplication/project-venv/simpleDjangoApp/simpleDjangoApp/manage.py migrate
