e7be1dc15de991a6f9bf882a2ade01a1cc4ba7cd
nikhilpatil29 <patilnikhil274@gmail.com>
2018-10-16 18:31:15 +0530

    16/10 night commited

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -3,4 +3,5 @@ cd /home/ec2-user/myApplication/project-venv/
 source /home/ec2-user/myApplication/project-venv/bin/activate
 # supervisorctl -c /home/ec2-user/www/project/supervisor/default.conf stop all 2&>1 >/dev/null
 # sudo unlink /tmp/supervisor.sock 2> /dev/null
-sudo pkill supervisor*
+ps aux | grep python | grep manage.py
+kill $(ps aux | grep python | grep manage.py | awk '{print $2}')
